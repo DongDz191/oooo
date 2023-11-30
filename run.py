63cc47566be0f9234b1c56ba83fbc5d8381e241b
PDong2501 <148313225+DongDz191@users.noreply.github.com>
2023-11-30 08:04:58 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
+import streamlit as st
 import os
 
-os.system("python detect.py --weights best.pt --img 416 --conf 0.5 --source 0")
+# Tiêu đề của ứng dụng
+st.title('Ứng Dụng Nhận Diện Đối Tượng')
+
+# Tạo một form cho phép người dùng nhập các tham số
+with st.form(key='my_form'):
+    weight_file = st.text_input(label='Đường dẫn tới file trọng số (best.pt)', value='best.pt')
+    image_size = st.number_input(label='Kích thước ảnh', value=416)
+    confidence_threshold = st.slider(label='Ngưỡng tự tin', min_value=0.0, max_value=1.0, value=0.5)
+    source = st.text_input(label='Nguồn (0 cho webcam, đường dẫn cho ảnh hoặc video)', value='0')
+    submit_button = st.form_submit_button(label='Chạy Nhận Diện')
+
+# Khi người dùng nhấn nút "Chạy Nhận Diện"
+if submit_button:
+    command = f"python detect.py --weights {weight_file} --img {image_size} --conf {confidence_threshold} --source {source}"
+    os.system(command)
